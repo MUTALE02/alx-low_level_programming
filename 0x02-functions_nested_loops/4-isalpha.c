69adf5936f8aff6cde5bbed75845a027b2eff5f5
MUTALE02 <edwin.zambia@outlook.com>
2022-12-14 09:48:39 -0800

    GATED

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - check for char alphabet
+* @c: the char to br checked
+* Return: 1 if char is lower or upper case letter, 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+	return (1);
+	else
+	return (0);
+}
