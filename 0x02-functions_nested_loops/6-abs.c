218153b25dd9143925f4a04d66ff1cada70eae2f
MUTALE02 <edwin.zambia@outlook.com>
2022-12-14 10:37:04 -0800

    HADE

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - computes the abs value of an integer
+* @n: the int in the task
+* Return: the absolute value
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+	return (n);
+	else
+	return (-n);
+}
