fab9ef9c5bf32819c95b64af7798444a63afab2a
MUTALE02 <edwin.zambia@outlook.com>
2022-12-14 13:03:37 -0800

    WATER

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* add - sum of 2 numbers
+* @a: 1st number
+* @b: 2nd number
+* Return: sum of the numbers
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* add - add  a sum 2 numbers
-* @a: 1st number
-* @b: 2nd number
-* Return: the sum of both
-*/
-int add(int a, int b)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	return (a + b);
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
 }
