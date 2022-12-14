8278b53bfa700e81a4c899505500ad5c601c2fdd
MUTALE02 <edwin.zambia@outlook.com>
2022-12-14 06:45:26 -0800

    WHERE

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print a-z 10x
+*
+*/
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+	for (la = 'a'; la <= 'z'; la++)
+	_putchar('\n');
+	}
+}
