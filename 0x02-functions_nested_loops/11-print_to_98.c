3b6665d47602756ddf84d4d0d250a898cf6f1e03
MUTALE02 <edwin.zambia@outlook.com>
2022-12-14 12:24:09 -0800

    DAKE

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* add - sum of 2 values
+* @n1: The first number is calculated
+* @n2: The second number is computed
+* Return: Int value
+*/
+int add(int n1, int n2)
+{
+	return (n1 +n2);
+}
