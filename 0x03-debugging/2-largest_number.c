20bb75ffae5ba9024dfdacac865e25089f2bf563
MUTALE02 <edwin.zambia@outlook.com>
2022-12-16 23:30:01 -0800

    QUAK

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,6 +12,8 @@
  * Return: largest number
  */
 
+
+
 int largest_number(int a, int b, int c)
 {
 
@@ -27,4 +29,5 @@ int largest_number(int a, int b, int c)
 	largest = c;
 
 	return (largest);
+
 }
