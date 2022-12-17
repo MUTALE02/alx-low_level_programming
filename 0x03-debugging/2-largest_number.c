47b48c4e2db66228126b77b3194bc31192ecfba6
MUTALE02 <edwin.zambia@outlook.com>
2022-12-17 02:03:53 -0800

    MADES

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,25 +2,21 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- *
  * @a: first integer
- *
  * @b: second integer
- *
  * @c: third integer
- *
  * Return: largest number
  */
+
+
 int largest_number(int a, int b, int c)
 {
-
-	int largest;
-
-	if (a > b && a > c)
+	int largest
+	if (a > b && b > c)
 	{
 	largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > a && a > c)
 	{
 	largest = b;
 	}
