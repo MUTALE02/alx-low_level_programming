32f7413d55cdc287f4d42a5729e5b8f854c27ef9
MUTALE02 <edwin.zambia@outlook.com>
2022-12-17 01:08:00 -0800

    WASBE

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -18,16 +18,19 @@ int largest_number(int a, int b, int c)
 {
 
 	int largest;
-
-	if (a > b && a > c)
-	largest = a;
-	else if (b > a && b > c)
-	largest = b;
 	
+	if (a > b && a > c)
+	{
+	largest = a;
+	}
+	else if (b > a && b > c)
+	{
+	largest = b;
+	}
 	else
-
+	{
 	largest = c;
-
+	}
 	return (largest);
 
 }
