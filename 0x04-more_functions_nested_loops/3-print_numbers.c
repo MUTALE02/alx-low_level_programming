ca7f35eb9a2373592db78bdc5b977a0481bbb0d7
MUTALE02 <edwin.zambia@outlook.com>
2022-12-18 08:30:53 -0800

    WOOL

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
-* print_numbers - print num from 0 - 9
+* print_numbers -> print numbers
 *
-* Return: void
+*
+*
 */
-
 void print_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar ('\n');
+	for (n = 0; n < 10; n++)
+
+	_putchar(n + '0');
+	_putchar('\n');
 }
