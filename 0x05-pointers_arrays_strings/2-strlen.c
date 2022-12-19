c829cc92f1209ff2a8ff1165583378a43eb911ca
MUTALE02 <edwin.zambia@outlook.com>
2022-12-19 09:16:21 -0800

    STREN

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/** _strlen - outputs the length of a string
+ * @s: char to check
+ *Return: 0
+ *
+ */
+
+int _strlen(char *s)
+{
+	int a = 0; //* start the count from gensis
+
+	for (; *s++;)
+	a++;
+
+	return (a);
+}
