c21f2e18e968457e1f702c518a8c6aac3dc99994
MUTALE02 <edwin.zambia@outlook.com>
2022-12-19 12:58:09 -0800

    KEPPA

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strcpy(char *dest, char *src)
+{
+int count = 0, i;
+
+	while *(src)
+	{
+	src++
+	count++;
+}
+
+	for (i = 0; i < count; i++)
+	src--;
+	for (i = 0; i < count; i++)
+	{
+	dest[i] = *src;
+	src++;
+	}
+	dest[i] = *src;
+
+	return (dest);
+}
