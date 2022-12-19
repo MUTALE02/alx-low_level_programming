2b03e6505e38721b75bc25ea648c2a43ffa34631
MUTALE02 <edwin.zambia@outlook.com>
2022-12-19 13:06:36 -0800

    KEPA5

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+* ptr = _strpy - (first, solve the problem. Then, write the code\n)
+* Return value: the pointer to dest
+*
+*/
 
 char *_strcpy(char *dest, char *src)
 {
