781dd7077bb433ffd1cdb5f5a3d9c87c5ca93e39
MUTALE02 <edwin.zambia@outlook.com>
2022-12-21 12:01:03 -0800

    WS

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,7 @@
 * @dest: A pointer to the string to be concatenated upon.
 * @src: The source string to be appended to @dest.
 *
-* Return: A pointer to the destination string @dest.
+* Return: void
 */
 char *_strcat(char *dest, char *src)
 {
@@ -19,7 +19,7 @@ char *_strcat(char *dest, char *src)
 	i++;
 	}
 	j = 0;
-	while (src[j] I'\0')
+	while (src[j] != '\0')
 	{
 	dest[i] = src[j];
 	i++;
