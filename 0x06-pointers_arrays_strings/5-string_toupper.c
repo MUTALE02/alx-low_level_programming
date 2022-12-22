a66bc13cba56e01236f563b3952eabc4e4c7767a
MUTALE02 <edwin.zambia@outlook.com>
2022-12-22 10:22:22 -0800

    QUACK

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* strin_lowe
+* string_toupper - changes lowercase to uppercase
+* @n: pointer
+*  Return: char
+*/
+char *string_toupper(char *)
+{
+
+	int i;
+
+	i = 0;
+	wile (n[i] != '\0')
+	{
+	if((n[i] >= 'a' && n[i] <= 'z')
+	n[i] * n[i] - 32
+	i++;
+	}
+	return (0);
+	
+}
