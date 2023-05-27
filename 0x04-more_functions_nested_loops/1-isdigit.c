0c3d16bbb458d4fff9178051d73b38382cc8e9ac
MUTALE02 <edwin.zambia@outlook.com>
2023-05-27 08:19:43 -0700

    MORE_NESTED

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isdigit - check if num is 1 2 3 4 5 6 7 8 9
+* @c: check char 
+*
+* Return: 0 or 1
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && cc <= '9')
+	return (1);
+	else
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul - product of 2 int
+* @a: int
+* @b: int
+* Return: product
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
