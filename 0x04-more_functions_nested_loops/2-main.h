ac1503b76dee42001992126e65b39330773e42de
MUTALE02 <edwin.zambia@outlook.com>
2023-05-27 02:04:50 -0700

    MORE_NESTED

--- 0x04-more_functions_nested_loops/2-main.h
+++ 0x04-more_functions_nested_loops/2-main.h
@@ -1,5 +1,4 @@
 #include "main.h"
-
 #include <stdio.h>
 
 /**
@@ -7,13 +6,9 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
-
 {
-	printf("%d\n", mul(98, 1024));
-	printf("%d\n", mul(-402, 4096));
-
-	return (0);
-
+    printf("%d\n", mul(98, 1024));
+    printf("%d\n", mul(-402, 4096));
+    return (0);
 }
