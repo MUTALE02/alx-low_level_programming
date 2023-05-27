8bb170ac4f328fcac3a17bdf1998927ae92dff42
MUTALE02 <edwin.zambia@outlook.com>
2023-05-27 08:40:41 -0700

    MORENs

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print - 0  9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char num;
+	for (num = '0'; num <= '9'; num++)
+{
+	_putchar(num);
+}
+	_putchar("\n");
+}
