3e948c31778ff2293f6f1e8a4a62680dae7bb69c
MUTALE02 <edwin.zambia@outlook.com>
2023-05-27 05:02:14 -0700

    MORE

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * C program to check whether a character is uppercase or lowercase 
+ *
+ * Return 0 or 1
+ */
+
+int _isupper(int c)
+{
+
+if(c >= 'A' && c <= 'Z')
+
+return 1;
+
+else
+
+return 0;
+}
