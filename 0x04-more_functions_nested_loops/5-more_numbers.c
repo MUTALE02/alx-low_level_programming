e3a78fafbf26822bf8fff90305652a237ed2863c
MUTALE02 <edwin.zambia@outlook.com>
2023-05-28 02:11:59 -0700

    NESTEDF

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int n, tens, units, row;
+
+	for (row = 1; row <= 10; row++)
+	{
+	for (n = 0; n <= 14; n++)
+	{
+	tens = n / 10;
+	units = n % 10;
+	if (n > 9)
+	_putchar(tens + '0');
+	_putchar(units + '0');
+	}
+	_putchar('\n');
+	}
+}
